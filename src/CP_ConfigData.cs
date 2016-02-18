d71f317a7f86053d6f1b7d447cc8a881a9bbd745
Wilco de Boer <deboer.wilco@gmail.com>
2016-02-18 08:26:33 +0100

    Add data class for config data section

---
+++ src/CP_ConfigData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Welder {
+    [Serializable]
+    class CP_ConfigData {
+        public string name = "NONE";
+        public string packageVersion = "";
+        public string sourceFolder = "";
+        public string destFolder = "";
+    }
+}
