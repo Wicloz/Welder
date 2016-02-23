8dd8d3318fe1a5a77e53b5d572698d39999202fc
Wilco de Boer <deboer.wilco@gmail.com>
2016-02-23 19:32:27 +0100

    Implement the exception manager UI

--- src/SB_ExceptionManager.Designer.cs
+++ src/SB_ExceptionManager.Designer.cs
@@ -24,20 +24,92 @@
         /// </summary>
         private void InitializeComponent () {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SB_ExceptionManager));
+            this.listViewExceptions = new System.Windows.Forms.ListView();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
+            // 
+            // listViewExceptions
+            // 
+            this.listViewExceptions.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewExceptions.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listViewExceptions.FullRowSelect = true;
+            this.listViewExceptions.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listViewExceptions.HideSelection = false;
+            this.listViewExceptions.Location = new System.Drawing.Point(13, 13);
+            this.listViewExceptions.MultiSelect = false;
+            this.listViewExceptions.Name = "listViewExceptions";
+            this.listViewExceptions.Scrollable = false;
+            this.listViewExceptions.Size = new System.Drawing.Size(220, 350);
+            this.listViewExceptions.TabIndex = 0;
+            this.listViewExceptions.UseCompatibleStateImageBehavior = false;
+            this.listViewExceptions.View = System.Windows.Forms.View.Details;
+            this.listViewExceptions.SelectedIndexChanged += new System.EventHandler(this.listViewExceptions_SelectedIndexChanged);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAdd.Location = new System.Drawing.Point(239, 12);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(220, 49);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Add Mod";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRemove.Location = new System.Drawing.Point(239, 67);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(220, 48);
+            this.buttonRemove.TabIndex = 2;
+            this.buttonRemove.Text = "Remove Mod";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxName.Location = new System.Drawing.Point(239, 121);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(220, 20);
+            this.textBoxName.TabIndex = 4;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Width = 9999;
             // 
             // SB_ExceptionManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(471, 375);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.listViewExceptions);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "SB_ExceptionManager";
             this.Text = "Ignored Mods Manager";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SB_ExceptionManager_FormClosing);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listViewExceptions;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
     }
 }
--- src/SB_ExceptionManager.cs
+++ src/SB_ExceptionManager.cs
@@ -12,16 +12,25 @@ using System.IO;
 namespace Welder {
     public partial class SB_ExceptionManager : Form {
         private static List<string> exceptions = new List<string>();
+        private int selectedIndex = -1;
+        private bool updatingData = false;
 
         public SB_ExceptionManager () {
             InitializeComponent();
             LoadExceptions();
+            ReloadExceptionList();
+        }
+
+        private void SB_ExceptionManager_FormClosing (object sender, FormClosingEventArgs e) {
+            SaveExceptions();
         }
 
         //Loads exception list
         private void LoadExceptions () {
             if (File.Exists(ServerBuilder.cd + "/server_exceptions.cfg"))
                 exceptions = SaveLoad.LoadFilePlain(ServerBuilder.cd + "/server_exceptions.cfg");
+            else
+                exceptions = CreateDefaultList();
             if (exceptions == null)
                 exceptions = new List<string>();
             SaveExceptions();
@@ -30,6 +39,87 @@ namespace Welder {
         //Saves exception list
         private void SaveExceptions () {
             SaveLoad.SaveFilePlain(exceptions, ServerBuilder.cd + "/server_exceptions.cfg");
+        }
+
+        //Reloads the listview with exceptions
+        private void ReloadExceptionList () {
+            updatingData = true;
+            listViewExceptions.Items.Clear();
+            listViewExceptions.SelectedIndices.Clear();
+            foreach (string mod in exceptions) {
+                ListViewItem lvi = new ListViewItem(mod);
+                listViewExceptions.Items.Add(lvi);
+            }
+            if (selectedIndex < exceptions.Count && selectedIndex > -1)
+                listViewExceptions.SelectedIndices.Add(selectedIndex);
+            SetInfoBox();
+            updatingData = false;
+        }
+
+        //Sets the content of the textbox based on the selection
+        private void SetInfoBox () {
+            if (selectedIndex >= 0) {
+                updatingData = true;
+                textBoxName.Text = exceptions[selectedIndex];
+                updatingData = false;
+            }
+        }
+
+        //Marks a new exception as selected and reloads the info box
+        private void listViewExceptions_SelectedIndexChanged (object sender, EventArgs e) {
+            if (!updatingData) {
+                if (listViewExceptions.SelectedIndices.Count > 0)
+                    selectedIndex = listViewExceptions.SelectedIndices[0];
+                else
+                    selectedIndex = -1;
+                SetInfoBox();
+            }
+        }
+
+        //Changes the name of the currently selected esception
+        private void textBoxName_TextChanged (object sender, EventArgs e) {
+            if (!updatingData && selectedIndex >= 0) {
+                exceptions[selectedIndex] = textBoxName.Text;
+                ReloadExceptionList();
+            }
+        }
+
+        //Adds a new exception with the current text
+        private void buttonAdd_Click (object sender, EventArgs e) {
+            exceptions.Add(textBoxName.Text);
+            selectedIndex = exceptions.Count - 1;
+            ReloadExceptionList();
+        }
+
+        //Removes the selected exception
+        private void buttonRemove_Click (object sender, EventArgs e) {
+            if (selectedIndex >= 0) {
+                exceptions.RemoveAt(selectedIndex);
+                if (selectedIndex >= exceptions.Count)
+                    selectedIndex -= 1;
+                ReloadExceptionList();
+            }
+        }
+
+        //Returns a list with some deafault exceptions
+        private List<string> CreateDefaultList () {
+            List<string> list = new List<string>();
+            list.Add("damageindicator");
+            list.Add("bacr");
+            list.Add("minimap");
+            list.Add("inventorytweaks");
+            list.Add("mapwriter");
+            list.Add("dynamiclights");
+            list.Add("shatter");
+            list.Add("bettertitlescreen");
+            list.Add("notenoughkeys");
+            list.Add("optifine");
+            list.Add("java7checker");
+            list.Add("java8checker");
+            list.Add("defaultkeys");
+            list.Add("neat");
+            list.Add("ding");
+            return list;
         }
 
         //Returns true when a mod should not be on a server
